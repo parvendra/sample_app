8aff695a5b0dffab8129332352834bbd5b5bcb72
parvendra <parvendra.s@multunus.com>
2010-12-13 18:02:13 +0530

    Finished first cut of the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,5 +20,6 @@ class User < ActiveRecord::Base
 
   
   validates :email, :presence => true,
-                    :format => { :with => email_regex}
+  :format => { :with => email_regex},
+  :uniqueness => { :case_sensitive => false }
 end
